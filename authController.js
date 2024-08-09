429cf565d6e229c28536faaa3022e6f3c2149622
Ced.dev <111016808+Cedrick-KC@users.noreply.github.com>
2024-08-09 16:08:23 +0200

    Add files via upload

---
+++ authController.js
@@ -0,0 +1,46 @@
+const User = require('../usermodel/user');
+const jwt =  require('jsonwebtoken');
+
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+}
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+return jwt.sign({id},'cedmade secret',{
+    expiresIn: maxAge
+});
+
+}
+module.exports.signup_get = (req,res) => {
+    res.render('sign')
+}
+
+module.exports.login_get = (req,res) => {
+    res.render('log')
+}
+
+module.exports.signup_post= async (req,res) => {
+    const{ username, email, password,dob } = req.body;
+    
+    try {
+        const user = await User.create({ username,email,password,dob });
+        const token = createToken(user._id);
+        res.cookie('jwt',token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({user: user._id});
+    }
+    catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
+}
+
+module.exports.login_post = async (req,res) => {
+    const { username,password } = req.body;
+try {
+  const user = await User.login ( username,password );
+  res.status(200).json({user: user._id});
+}
+
+catch (err) {
+
+}}
--- user.js
+++ user.js
@@ -1,24 +1,61 @@
+const { lowerCase } = require('lodash');
+const express = require ('express');
 const mongoose = require ('mongoose');
+const {isEmail}= require ('validator');
+const bcrypt = require ('bcryptjs')
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
     username: {
         type: String,
-        required:true
+        required:[true,'Please enter valid username'],
+        unique:[true,'Username is not available'],
     },
     email: {
         type: String,
-        required: true
+        required:[true,'Please enter valid email adress'],
+        unique:true,
+        lowerCase:true,
+        validate:[isEmail,'Please enter valid E mail']
     },
     password: {
         type: String,
-        required: true
+        required: [true,'Please enter valid password with atleast 6 characters'],
+        minlength: 6
     },
     dob: {
         type:String,
-        required: true
+        required: [true,'Please enter valid date of birth']
     }
-},
- { timestamps: true});
+});
+
+//{ timestamps: true});
 const User = mongoose.model('User',userSchema);
+userSchema.pre ('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+//hashing
+
+
+    
+    
+    userSchema.statics.login = async function(username,password)  {
+        const user = await this.findOne({ username });
+        if (user)  {
+            const auth = await bcrypt.compare(password,user.password)
+            if  (auth) {
+                return user
+            }
+            throw Error ('Incorrect password')
+        }
+        throw Error('Incorrect Username')
+
+    }
+
+   
+
+
+
 module.exports = User;
