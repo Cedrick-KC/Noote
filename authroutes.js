1502339b4f9ce157dce525c0cc79ada31b738cd3
Ced.dev <111016808+Cedrick-KC@users.noreply.github.com>
2024-08-10 11:19:00 +0200

    Add files via upload

---
+++ authroutes.js
@@ -0,0 +1,11 @@
+const { Router } = require ('express');
+const authController = require ('../Controllers/authController');
+
+const router = Router();
+
+router.get('/sign', authController.signup_get);
+router.post('/sign', authController.signup_post);
+router.get('/log', authController.login_get);
+router.post('/log', authController.login_post);
+
+module.exports = router;
