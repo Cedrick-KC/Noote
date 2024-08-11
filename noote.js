53293d7306a1edf6626f75dcad77d7224c2bcd96
Ced.dev <111016808+Cedrick-KC@users.noreply.github.com>
2024-08-11 12:13:50 +0200

    Update noote.js

--- noote.js
+++ noote.js
@@ -1,3 +1,4 @@
+export const noote = {
 const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 const noteSchema = new Schema( {
@@ -18,6 +19,7 @@ content: {
 
 
 const Note = mongoose.model('Note',noteSchema);
-
+};
 module.exports = Note;
+
 export default noote;
