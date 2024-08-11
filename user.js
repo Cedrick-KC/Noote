97f36051c44ef4bde4b01e75d28944e3ce3fb240
Ced.dev <111016808+Cedrick-KC@users.noreply.github.com>
2024-08-11 12:17:18 +0200

    Update user.js

--- user.js
+++ user.js
@@ -1,3 +1,4 @@
+export const user = {
 const { lowerCase } = require('lodash');
 const express = require ('express');
 const mongoose = require ('mongoose');
@@ -53,6 +54,7 @@ userSchema.pre ('save', async function (next) {
         throw Error('Incorrect Username')
 
     }
+};
 
    
 
