d35a0f01db0ce17d82b4a6aaac8d41009d11ae15
Ced.dev <111016808+Cedrick-KC@users.noreply.github.com>
2024-08-11 10:56:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const MongoStore = require ('connect-mongo');
 const cookieparser = require('cookie-parser');
-const Note = require('noote.js');
-const User = require('user.js');
-const authRoutes = require ('authroutes.js');
+import Note from ('noote.js');
+import User from ('user.js');
+import authRoutes from  ('authroutes.js');
 const jwt = require ('jsonwebtoken');
 
 
